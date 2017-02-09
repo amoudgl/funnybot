1a4bf6ee5d5d81f71d2f08d871393c50437a054f
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2017-02-09 15:45:24 +0530

    Added support for download from url

--- utils/csv_to_text.py
+++ utils/csv_to_text.py
@@ -1,7 +1,23 @@
 # Generates a text file from csv, removing serial numbers
-jokes = open('../data/all-jokes.csv').readlines()
-f = open('../data/all-jokes.txt', 'a')
-for i in xrange(1, len(jokes)):
-    joke = jokes[i].split(',',1)[1]
-    f.write(joke)
+import csv
+import urllib
+import zipfile
+import os 
+
+try: 
+    filepath = '../data/shortjokes.csv'
+    data_file = open(filepath)
+except: 
+    print ("Dataset not found in ../data/ directory!")
+    print ("Downloading...")
+    url = 'https://github.com/abhinavmoudgil95/short-jokes-dataset/raw/master/shortjokes.csv'
+    urllib.urlretrieve (url, "../data/shortjokes.csv")
+    print ("Successfully downloaded: ../data/shortjokes.csv")
+    data_file = open('../data/shortjokes.csv')
+
+reader = csv.reader(data_file)
+next(reader, None)
+f = open('../data/shortjokes.txt', 'w')
+for row in reader: 
+    f.write(row[1] + "\n\n")
 f.close()
